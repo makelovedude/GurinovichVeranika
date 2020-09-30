52ebb6b790e64adee61e2f409b3e11baca2e0b66
makelovedude <logivi259270613>
2020-09-30 16:39:53 +0300

    commit

--- Task21/script.js
+++ Task21/script.js
@@ -1,1 +1,22 @@
+var car = [];
+function create_an_array() {
+  for(var i = 0; i < 5; i++)
+  car.push(prompt("Введите, пожалуйста, пять(5) марок машин, которые Вы знаете, чтобы добавить их в массив."));
+}
+console.log(car); 
+ 
+function delete_an_array() {
+  car.length = 0; 
+} 
 
+function check_an_array() {
+  var check = prompt("Введите марку машины, чтобы проверить ее в массиве.");
+   
+  if (car.includes(check) ) {
+    alert("Эта марка машины числится под индексом " + car.indexOf(check));
+  } else {
+    alert("Такой марки еще нет.")
+    car.push(check);
+    alert("Новая марка машины добавлена.")
+  }
+}
